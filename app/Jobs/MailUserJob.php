f6a88a38c710517002e6fedcbce41c3f0fbbed9c
Stanislav Tilt <stasispuha@gmail.com>
2026-07-10 13:42:52 +0200

    fix

--- app/Jobs/MailUserJob.php
+++ app/Jobs/MailUserJob.php
@@ -3,15 +3,19 @@
 namespace App\Jobs;
 
 use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 
-class MailUserJob implements ShouldQueue
+class MailUserJob implements ShouldQueue, ShouldBeUnique
 {
-    use Queueable;
+    use Queueable, SerializesModels;
 
+    public $tries = 3;
+    public $backoff = 60;
     /**
      * Create a new job instance.
      */
